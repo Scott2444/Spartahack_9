e0f3b5d694ac6a156a1b7ddff84036ed58d85790
ngoaaro1 <ngoaaro1@msu.edu>
2024-01-28 02:32:36 -0500

    COMPLETED FRONT END

--- flask-server/server.py
+++ flask-server/server.py
@@ -4,7 +4,8 @@ app = Flask(__name__)
 
 @app.route("/members")
 def members():
-    return{"members": ["Member1", "Member2", "Member3"]}
+    return[{"district":"1","party":"democrat"},
+            {"district":"2","party":"republican"}]
 
 if __name__ == "__main__":
     app.run(debug=True)
