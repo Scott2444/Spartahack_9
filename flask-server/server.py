f9733ca3dee6f0fc83a28e16ae16f8868f27575e
ngoaaro1 <ngoaaro1@msu.edu>
2024-01-28 09:47:20 -0500

    district display results

--- flask-server/server.py
+++ flask-server/server.py
@@ -5,7 +5,8 @@ app = Flask(__name__)
 @app.route("/members")
 def members():
     return[{"district":"1","party":"democrat"},
-            {"district":"2","party":"republican"}]
+            {"district":"2","party":"republican"},
+            {"district":"11","party":"republican"}]
 
 if __name__ == "__main__":
     app.run(debug=True)
